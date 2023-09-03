48a80a9dcfb2bdae4e06874312ca1a37eea715e0
Gaston Bustamante <gaston82@hotmail.com>
2023-09-03 01:56:36 -0300

    Agregado de FlatList y Modal junto con el manejo de los eventos correspondientes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import {
-	Button,
+	FlatList,
+	Modal,
 	Pressable,
 	StyleSheet,
 	Text,
@@ -9,11 +10,84 @@ import {
 	View,
 } from "react-native";
 import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
+import { useState } from "react";
 
 export default function App() {
+	const initialList = [
+		{
+			id: 1,
+			name: "Fideos",
+		},
+		{
+			id: 2,
+			name: "Gaseosas",
+		},
+		{
+			id: 3,
+			name: "Manteca",
+		},
+	];
+
+	const [inputText, setInputText] = useState("");
+
+	const [list, setList] = useState(initialList);
+
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
+	const onAddItem = () => {
+		setList([
+			...list,
+			{
+				id: new Date().getTime() + inputText,
+				name: inputText,
+			},
+		]);
+		setInputText("");
+	};
+
+	const onDeleteItem = (id) => {
+		setList(list.filter((item) => item.id !== id));
+	};
+
+	const onClearList = () => {
+		setList([]);
+    setIsModalVisible(false);
+	};
+
 	return (
 		<View style={styles.container}>
 			{/* <StatusBar style="auto" /> */}
+			<Modal
+				animationType="none"
+				visible={isModalVisible}
+				transparent={true}
+				onRequestClose={() => {
+					Alert.alert("Modal has been closed.");
+					setIsModalVisible(!isModalVisible);
+				}}
+			>
+				<View style={styles.deleteAllItemsModalContainer}>
+					<View style={styles.deleteAllItemsModal}>
+						<Text style={styles.deleteAllItemsModalText}>
+							¿Está seguro de querer eliminar todos los elementos de la lista?
+						</Text>
+						<View style={styles.modalButtonsContainer}>
+							<Pressable
+								style={styles.confirmDeleteAllItemsButton}
+								onPress={() => setIsModalVisible(false)}
+							>
+								<Text style={styles.cancelDeleteAllItemsText}>Cancelar</Text>
+							</Pressable>
+							<Pressable
+								style={styles.confirmDeleteAllItemsButton}
+								onPress={() => onClearList()}
+							>
+								<Text style={styles.confirmDeleteAllItemsText}>OK</Text>
+							</Pressable>
+						</View>
+					</View>
+				</View>
+			</Modal>
 			<View style={styles.header}>
 				<FontAwesome5 name="clipboard-list" size={48} color="black" />
 				<Text style={styles.headerText}>Lista de compras</Text>
@@ -23,42 +97,41 @@ export default function App() {
 					<TextInput
 						placeholder="Ingrese un producto... "
 						style={styles.textInput}
+						value={inputText}
+						onChangeText={(text) => {
+							setInputText(text);
+						}}
 					/>
-					<Pressable
-						style={styles.inputButton}
-						onPress={() => console.log(new Date().getTime())}
-					>
+					<Pressable style={styles.inputButton} onPress={onAddItem}>
 						<AntDesign name="pluscircle" size={36} color="black" />
 					</Pressable>
 				</View>
 				<View style={styles.listContainer}>
-					<View style={styles.listItem}>
-						<Text style={styles.itemText}>Manzanas</Text>
-						<Pressable
-							style={styles.deleteItemButton}
-							onPress={() => console.log("Se presionó Eliminar Item")}
-						>
-							<AntDesign name="delete" size={24} color="black" />
-						</Pressable>
-					</View>
+					<FlatList
+						data={list}
+						renderItem={({ item }) => (
+							<View style={styles.listItem} key={item.id}>
+								<Text style={styles.itemText}>{item.name}</Text>
+								<Pressable
+									style={styles.deleteItemButton}
+									onPress={() => onDeleteItem(item.id)}
+								>
+									<AntDesign name="delete" size={24} color="gray" />
+								</Pressable>
+							</View>
+						)}
+						keyExtractor={(item) => item.id}
+					/>
 				</View>
 			</View>
 			<View style={styles.footer}>
-				{/* <Button
-					title="Vaciar"
-					style={styles.deleteAllButton}
-					onPress={() => {
-						console.log("Se presionó Vaciar");
-					}}
-					color="#97d000"
-				/> */}
 				<TouchableHighlight
 					style={styles.deleteAllButton}
 					activeOpacity={0.6}
 					underlayColor="#DDDDDD"
-					onPress={() => alert("Pressed!")}
+					onPress={() => setIsModalVisible(true)}
 				>
-					<Text style={styles.deleteAllButtonText}>Vaciar</Text>
+					<Text style={styles.deleteAllButtonText}>Limpiar</Text>
 				</TouchableHighlight>
 			</View>
 		</View>
@@ -66,9 +139,43 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+	deleteAllItemsModalContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "stretch",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semitransparente
+	},
+	deleteAllItemsModal: {
+		backgroundColor: "#444444",
+		margin: 45,
+		borderRadius: 20,
+		padding: 15,
+	},
+	deleteAllItemsModalText: {
+    margin: 5,
+		fontSize: 20,
+    color: "#dddddd",
+	},
+  modalButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  confirmDeleteAllItemsButton: {
+    padding: 5,
+  },
+  cancelDeleteAllItemsText: {
+    fontSize: 20,
+    color: "#97d000",
+  },
+  confirmDeleteAllItemsText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#97d000",
+  },
 	container: {
 		flex: 1,
-		alignItems: "center",
+		alignItems: "stretch",
 		justifyContent: "space-between",
 	},
 	header: {
@@ -84,7 +191,7 @@ const styles = StyleSheet.create({
 		fontSize: 36,
 	},
 	body: {
-		flex: 0.8,
+		flex: 0.85,
 		justifyContent: "flex-start",
 		alignContent: "center",
 	},
@@ -99,7 +206,7 @@ const styles = StyleSheet.create({
 		borderBottomWidth: 2,
 		width: "75%",
 		fontSize: 20,
-		color: "#aaaaaa",
+		color: "#111111",
 	},
 	inputButton: {},
 	listContainer: {
@@ -110,25 +217,29 @@ const styles = StyleSheet.create({
 	listItem: {
 		flexDirection: "row",
 		justifyContent: "space-between",
-		marginTop: 10,
+		marginTop: 15,
 	},
 	itemText: {
-		fontSize: 20,
+		fontSize: 16,
 	},
 	deleteItemButton: {
 		marginRight: 6,
 	},
 	deleteAllButton: {
-		marginTop: 10,
-		backgroundColor: "#97d000",
-		padding: 15,
-		borderRadius: 15,
+		backgroundColor: "#ffcc00",
+		paddingTop: 5,
+		paddingBottom: 5,
+		paddingLeft: 15,
+		paddingRight: 15,
+		borderRadius: 25,
 	},
 	deleteAllButtonText: {
-		fontSize: 24,
+		fontSize: 20,
 	},
 	footer: {
-		flex: 0.2,
+		flex: 0.15,
 		justifyContent: "center",
+		backgroundColor: "#97d000",
+		alignItems: "center",
 	},
 });
